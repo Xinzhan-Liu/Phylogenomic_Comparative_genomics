bafbf7b80f61e227d5a6ab5092aa110ac66821d2
Xin-Zhan Liu <114778523+Xinzhan-Liu@users.noreply.github.com>
2023-09-29 19:34:13 -0700

    Create Synteny-gggenomes.R

---
+++ Synteny/Synteny-gggenomes.R
@@ -0,0 +1,10 @@
+library(gggenomes)
+
+# to inspect the example data shipped with gggenomes
+data(package="gggenomes")
+
+# There are 8 datasets in package 'gggenomes'
+# to inspect each dataset using 'data.frame()'
+data.frame(emale_genes)
+
+#
