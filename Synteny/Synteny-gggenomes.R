92c1b6b21dc0edc85483d8e0e26cc5670e62601e
Xin-Zhan Liu <114778523+Xinzhan-Liu@users.noreply.github.com>
2023-09-29 20:42:06 -0700

    Update Synteny-gggenomes.R

--- Synteny/Synteny-gggenomes.R
+++ Synteny/Synteny-gggenomes.R
@@ -4,7 +4,7 @@ library(gggenomes)
 data(package="gggenomes")
 
 # There are 8 datasets in package 'gggenomes'
-# to inspect each dataset using 'data.frame()'
+# to inspect each dataset using 'data.frame()'，查看每一个数据集
 data.frame(emale_genes)
 
 #
