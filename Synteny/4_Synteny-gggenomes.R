39eb806ef379c61ba5776d6a26a58dc064a9905f
Xin-Zhan Liu <114778523+Xinzhan-Liu@users.noreply.github.com>
2023-10-08 16:42:15 -0700

    Update 4_Synteny-gggenomes.R

--- Synteny/4_Synteny-gggenomes.R
+++ Synteny/4_Synteny-gggenomes.R
@@ -7,4 +7,11 @@ data(package="gggenomes")
 # to inspect each dataset using 'data.frame()'，查看每一个数据集
 data.frame(emale_genes)
 
-#
+#load the seqs file
+s0 <- read_table("seqs.txt")
+
+#load the genes file
+g0 <- read.table("3_test.csv", sep = ",", header = TRUE)
+
+#add two tracks including seqs and genes
+gggenomes(g0,s0)+geom_seq()+geom_gene()+geom_gene_tag(aes(label=name))
